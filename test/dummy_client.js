1763c303faee62ddd2edf18a971e7d33a74b2109
TheAncientOwl <dele.alex.o.o@gmail.com>
2024-06-05 23:13:31 +0300

    Implement basic websocket client

---
+++ test/dummy_client.js
@@ -0,0 +1,23 @@
+const WebSocket = require('ws');
+
+const socket = new WebSocket('ws://localhost:8083');
+
+socket.on('open', () => {
+  console.log('Connected to the WebSocket server');
+
+  const message = 'Hello from Node.js!';
+  console.log('Sending:', message);
+  socket.send(message);
+});
+
+socket.on('message', (data) => {
+  console.log('Received:', data.toString());
+});
+
+socket.on('close', () => {
+  console.log('Disconnected from the WebSocket server');
+});
+
+socket.on('error', (err) => {
+  console.error('WebSocket error:', err);
+});
