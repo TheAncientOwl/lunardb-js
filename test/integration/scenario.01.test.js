6311d1d95a0bee1468c847304f54c04826f794c5
TheAncientOwl <dele.alex.o.o@gmail.com>
2024-06-06 03:16:23 +0300

    Update integration scenario01

--- test/integration/scenario.01.test.js
+++ test/integration/scenario.01.test.js
@@ -8,27 +8,28 @@ const test = () => {
   ldb.connect();
 
   const scenarioCallback = () => {
-    ldb.execQuery('drop database DummyDB;').afterQueryExec(result => {
-      Logger.info(`0. ${result}`);
+    ldb.execQuery('database drop DummyDB;').afterQueryExec(result => {
+      Logger.info(`[RESPONSE] Step 0. ${result}`);
       ldb.execQuery('database create DummyDB;').afterQueryExec(result => {
-        Logger.info(`1. ${result}`);
+        Logger.info(`[RESPONSE] Step 1. ${result}`);
 
-        ldb.execQuery('use database DummyDB;').afterQueryExec(result => {
-          Logger.info(`2. ${result}`);
+        ldb.execQuery('database use DummyDB;').afterQueryExec(result => {
+          Logger.info(`[RESPONSE] Step 2. ${result}`);
 
           ldb.execQuery('schema Employee { name: String; salary: float; };').afterQueryExec(result => {
-            Logger.info(`3. ${result}`);
+            Logger.info(`[RESPONSE] Step 3. ${result}`);
 
             ldb.execQuery('create collection Employees based on Employee;').afterQueryExec(result => {
-              Logger.info(`4. ${result}`);
+              Logger.info(`[RESPONSE] Step 4. ${result}`);
 
               ldb
                 .execQuery(
                   'insert into Employees objects [ {"name":"Bob","salary":"4000"}{"name":"George","salary":"4500"} ];'
                 )
                 .afterQueryExec(result => {
-                  Logger.info(`5. ${result}`);
+                  Logger.info(`[RESPONSE] Step 5. ${result}`);
                   ldb.removeOnConnectListener(scenarioCallback);
+                  ldb.disconnect();
                 });
             });
           });
